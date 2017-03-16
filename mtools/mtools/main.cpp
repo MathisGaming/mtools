a3ea7e92a8b084e31ae4bcf0ef72de153089a8de
Mathis <mathis@helm-systems.com>
2017-03-15 21:43:17 -0400

    forgot to push main

--- mtools/mtools/main.cpp
+++ mtools/mtools/main.cpp
@@ -1,11 +1,16 @@
 #include "stdafx.h"
 #include "tree.h"
+#include "SortingAlgorithms.h"
+#include "LinkedList.h"
+
 #include <string.h>
 #include <iostream>
+#include <stack>
+#include <queue>
 
 // this implicitly goes to the ascii table and return
 // the numerical value affected to the char.
-int ctoi(char str)
+inline int ctoi(char str)
 {
 	return str;
 }
@@ -23,16 +28,44 @@ long long strtoint(char* str)
 	return total;
 }
 
-int main()
-{
+void test_trees() {
 	mtools::CTree<int>* myTree = new mtools::CTree<int>();
 	myTree->insert(2);
 	myTree->insert(3);
 	myTree->insert(7);
 	myTree->insert(1);
+	int size = myTree->get_height();
+	std::cout << size << std::endl;
 
-	std::cout << strtoint("ABCDEFGH") << std::endl;
+	myTree->insert(8);
+
+	size = myTree->get_height();
+	std::cout << size << std::endl;
+	myTree->display_pre_order();
+
+	delete myTree;
+	myTree = NULL;
+}
+
+void test_buble_sort() {
+	std::vector<int> v;
+	//v.push_back(7); v.push_back(5); v.push_back(9); v.push_back(8); v.push_back(2); v.push_back(3); v.push_back(1);
+	v.push_back(3); v.push_back(2); v.push_back(1);
+	for (auto i : v) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+	mtools::buble_sort(v);
+	for (auto i : v) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	test_trees();
+	test_buble_sort();
 
     return 0;
 }
-
